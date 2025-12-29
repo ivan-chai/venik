5d1ef70f7288bd5221edcf3a2dce5ff977101a8b
Ivan Karpukhin <karpuhini@yandex.ru>
2025-12-29 10:11:33 +0300

    Add list-sweeps tool

---
+++ venik/list_sweeps.py
@@ -0,0 +1,26 @@
+import argparse
+import optuna
+import os
+import random
+import string
+import yaml
+from mlflow.tracking import MlflowClient
+
+from .utils import SweepDB
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser("List sweeps. Use environment variables for locating Optuna.")
+    args = parser.parse_args()
+    return args
+
+
+def list_sweeps(args):
+    db = SweepDB()
+    for r in sorted(db.get_sweeps_list(), key=lambda r: r["sweep_id"]):
+        print(r["sweep_id"])
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    list_sweeps(args)
